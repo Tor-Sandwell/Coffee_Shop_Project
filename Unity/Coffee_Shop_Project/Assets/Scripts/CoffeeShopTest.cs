6c177234ae0938d0dc5988a0ab729b6fd881dc85
Tor-Sandwell <20293344@farn-ct.ac.uk>
2026-09-10 16:33:57 +0100

    feat: Coffees ordered script

---
+++ Unity/Coffee_Shop_Project/Assets/Scripts/CoffeeShopTest.cs
@@ -0,0 +1,22 @@
+    using UnityEngine;
+
+public class CoffeeShopTest : MonoBehaviour
+{
+    public int CoffeesSold = 6;
+    public float CoffeePrice =3.50f;
+    public int CoffeesOrdered = 3;
+    void Start()
+    {
+        Debug.Log("Hello World");
+
+        Debug.Log("I have ordered "+ CoffeesOrdered + " Coffees");
+        AddCoffee(CoffeesOrdered);
+        Debug.Log("Coffees sold: " + CoffeesSold);
+    }
+
+    void AddCoffee(int coffeesOrdered)
+    {
+        CoffeesSold += coffeesOrdered;
+    }
+
+}
